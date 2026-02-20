9f1174df5f11b2871a65e8c8f4142a29f9cf2604
ChxraWish <99240314+First364600@users.noreply.github.com>
2026-02-20 23:49:29 +0700

    Update firebase.ts

--- apps/lab06_firebase/src/firebase.ts
+++ apps/lab06_firebase/src/firebase.ts
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "d253e1ad0e72421cc372e814caa3bf87b3747333",
-    authDomain: "https://accounts.google.com/o/oauth2/auth",
-    projectId: "mobile-and-web-7a140"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    projectId: "xxx"
 };
 
 const app = initializeApp(firebaseConfig);
