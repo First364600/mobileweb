c27a47ca1e030550ad0789d86968ecb6d26e6f9c
First364600 <sakdichai1312547@outlook.com>
2026-02-23 16:44:31 +0700

    lab09-sensors

---
+++ docs/lab09-sensors/assets/web-CI2rCgAb.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-Bq7UPu6Q.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
